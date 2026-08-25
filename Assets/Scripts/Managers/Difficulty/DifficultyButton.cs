4f016369dc0457e0f0a7791166c8ddd7ba1e0600
scion <60977346+ryan-cary@users.noreply.github.com>
2026-08-25 11:50:19 -0700

    Update main menu
    - Add selected difficulty display
    - Fix game starting on mouseover play button

--- Assets/Scripts/Managers/Difficulty/DifficultyButton.cs
+++ Assets/Scripts/Managers/Difficulty/DifficultyButton.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class DifficultyButton : MonoBehaviour
 {
     [SerializeField] Difficulty desiredDifficulty;
+	[SerializeField] TMP_Text difficultyText;
 	
 	public void SelectDifficulty()
 	{ GameManager.GetInstance().GetDifficultyManager().GetComponent<DifficultySelector>().SetDifficulty(desiredDifficulty); }
+	
+	public void UpdateText()
+	{ difficultyText.text = $"Selected: {desiredDifficulty}"; }
 }
