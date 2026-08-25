afeffb12accb9ba9538b8da3856fff1f8c27aa1d
scion <60977346+ryan-cary@users.noreply.github.com>
2026-08-24 17:21:13 -0700

    Add difficulty switching to menu buttons

--- Assets/Scripts/Managers/Difficulty/DifficultyButton.cs
+++ Assets/Scripts/Managers/Difficulty/DifficultyButton.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 
+[RequireComponent(typeof(UnityEngine.UI.Button))]
 public class DifficultyButton : MonoBehaviour
 {
     [SerializeField] Difficulty difficultyToLoad;
+	
+	void Start()
+	{ this.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(LoadDifficulty); }
 	
 	private void LoadDifficulty()
 	{ GameManager.GetInstance().GetDifficultyManager().GetComponent<DifficultySelector>().SetDifficulty(difficultyToLoad); }
